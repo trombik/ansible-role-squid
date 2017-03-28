501a79135d298a1aea45b2ea5de5631b57dc510a
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-03-28 22:07:37 +0900

    initial implementation

--- tests/serverspec/default_spec.rb
+++ tests/serverspec/default_spec.rb
@@ -1,28 +1,50 @@
-require 'spec_helper'
-require 'serverspec'
+require "spec_helper"
 
-package = 'squid'
-service = 'squid'
-config  = '/etc/squid/squid.conf'
-user    = 'squid'
-group   = 'squid'
-ports   = [ PORTS ]
-log_dir = '/var/log/squid'
-db_dir  = '/var/lib/squid'
+package = "squid"
+service = "squid"
+config  = "/etc/squid/squid.conf"
+user    = "squid"
+group   = "squid"
+ports   = [ 3128 ]
+log_dir = "/var/log/squid"
+cache_dir  = "/var/lib/squid/cache"
+coredump_dir = "/var/squid/cache"
+default_user = "root"
+default_group = "root"
 
 case os[:family]
-when 'freebsd'
-  config = '/usr/local/etc/squid.conf'
-  db_dir = '/var/db/squid'
+when "freebsd"
+  config = "/usr/local/etc/squid/squid.conf"
+  cache_dir = "/var/squid/cache"
+  default_group = "wheel"
 end
 
 describe package(package) do
   it { should be_installed }
 end 
 
+describe file(cache_dir) do
+  it { should be_directory }
+  it { should be_owned_by user }
+  it { should be_grouped_into group }
+  it { should be_mode 750 }
+end
+
+describe file("#{ cache_dir }/00") do
+  it { should be_directory }
+  it { should be_owned_by user }
+  it { should be_grouped_into group }
+  it { should be_mode 750 }
+end
+
 describe file(config) do
   it { should be_file }
-  its(:content) { should match Regexp.escape('squid') }
+  it { should be_owned_by default_user }
+  it { should be_grouped_into default_group }
+  it { should be_mode 644 }
+  its(:content) { should match(/^http_port\s+3128$/) }
+  its(:content) { should match(/^cache_dir ufs #{ Regexp.escape(cache_dir) } 100 16 256$/) }
+  its(:content) { should match(/^coredump_dir #{ Regexp.escape(coredump_dir) }$/) }
 end
 
 describe file(log_dir) do
@@ -32,17 +54,14 @@ describe file(log_dir) do
   it { should be_grouped_into group }
 end
 
-describe file(db_dir) do
-  it { should exist }
-  it { should be_mode 755 }
-  it { should be_owned_by user }
-  it { should be_grouped_into group }
-end
-
 case os[:family]
-when 'freebsd'
-  describe file('/etc/rc.conf.d/squid') do
+when "freebsd"
+  describe file("/etc/rc.conf.d/squid") do
     it { should be_file }
+    it { should be_mode 644 }
+    it { should be_owned_by default_user }
+    it { should be_grouped_into default_group }
+    its(:content) { should match(/^squid_flags="-u 3180"$/) }
   end
 end
 
